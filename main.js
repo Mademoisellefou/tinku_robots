2f7ba9e34d0332e6ea31f307f79a2ab5ded8a942
melany.estevez <mel.estevez99@gmail.com>
2023-11-26 07:59:15 -0400

    fix: adicion base

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 var path = require('path')
+var bodyParser = require('body-parser')
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 app.set('views', path.join(__dirname, 'views'))
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json())
 app.get('/upload', function(req, res) {
   res.render('pages/index');
 });
