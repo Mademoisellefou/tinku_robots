a676bf9dad46d7567231cd626530d35abb976a87
melany.estevez <mel.estevez99@gmail.com>
2023-11-26 07:28:21 -0400

    fix: adicion base

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ app.set('views', path.join(__dirname, 'views'))
 app.get('/upload', function(req, res) {
   res.render('pages/index');
 });
-app.get('/upload', function(req, res) {
-  res.render('pages/index');
+app.post('/upload', function(req, res) {
+  console.log(res);
+  res.redirect('/upload')
 });
 app.listen(8000);
 console.log('Server is listening on port 8080');
